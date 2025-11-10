d6846294bfd5881c0b68b18268603d8987b2a3cf
Robin <62151722+IK-Robin@users.noreply.github.com>
2025-11-10 14:43:37 +0600

    change the file name

--- assets/js/rcost-interactive-single-function.js
+++ assets/js/rcost-interactive-single-function.js
@@ -101,7 +101,11 @@
       // example: just log instead of redirect
       console.log("Clicked lot:", mapD.id, "->", mapD.link);
       // window.location.href = mapD.link;
-      window.location.href = '../all nods/node-1.html';
+     window.location.href = 'all-nodes/node-1.html';  // No ../ needed
+      // get the home url  
+
+       
+
     }
 
     // Init default colors if provided
